0f449e2741d0403d9b10ad6256d5f2383ad9e5b2
Adil Dyer <adil@Adils-Macbook-Air.local>
2024-04-07 23:09:51 +0530

    design changes

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ const { studentSchema } = require("./schema");
 const Student = require("./models/student");
 const Recruiter = require("./models/recruiter");
 const passport = require("passport");
+// const twilio = require("twilio");
+const nodemailer = require("nodemailer");
 const LocalStrategy = require("passport-local");
 // const { isAuthenticated } = require("./middleware");
 const store = MongoStore.create({
@@ -39,6 +41,7 @@ const sessionOptions = {
     httpOnly: true,
   },
 };
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
@@ -48,7 +51,7 @@ app.use(express.static(path.join(__dirname, "/public")));
 
 app.use(session(sessionOptions));
 app.use(flash());
-
+// const client = twilio(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(Student.authenticate()));
@@ -73,11 +76,27 @@ async function main() {
 
 let isAuthenticated = async (req, res, next) => {
   if (req.isAuthenticated()) {
-    res.locals.isAuthenticated = true; // Set a variable indicating the user is authenticated
+    res.locals.isAuthenticated = true; // Set a variable indicating the user
   } else {
     res.locals.isAuthenticated = false;
   }
-  next();
+  return next();
+};
+
+let isLoggedIn = async (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    req.flash("error", "You must be logged in first !");
+    res.redirect("/");
+  }
+  return next();
+};
+
+let shallNotAuthenticated = async (req, res, next) => {
+  if (req.isAuthenticated()) {
+    req.flash("error", "You Must Log out First !");
+    res.redirect("/");
+  }
+  return next();
 };
 
 app.use(isAuthenticated);
@@ -90,7 +109,7 @@ app.get("/", (req, res) => {
   res.render("index.ejs", { isAuthenticated: res.locals.isAuthenticated });
 });
 
-app.get("/login-student", (req, res) => {
+app.get("/login-student", shallNotAuthenticated, (req, res) => {
   res.render("loginstu.ejs");
 });
 app.post(
@@ -113,12 +132,16 @@ app.get("/logout", function (req, res, next) {
   });
 });
 app.get("/error", (req, res) => {
-  res.send("some error occurred !");
+  req.flash("error", "some error occurred !");
+  res.redirect("/");
 });
-app.get("/login-recruiter", (req, res) => {
+app.get("/login-recruiter", shallNotAuthenticated, (req, res) => {
   res.render("loginrec.ejs");
 });
 
+app.get("/account", isLoggedIn, (req, res) => {
+  res.send("in preparation");
+});
 app.get("/placement-team", (req, res) => {
   res.render("placementTeam.ejs");
 });
@@ -133,7 +156,7 @@ app.get("/student", (req, res) => {
 app.get("/resrec", (req, res) => {
   res.render("resrecru.ejs");
 });
-app.get("/register/:user", (req, res) => {
+app.get("/register/:user", shallNotAuthenticated, (req, res) => {
   let { user } = req.params;
 
   if (user == "rec") {
@@ -145,7 +168,7 @@ app.get("/register/:user", (req, res) => {
   }
 });
 
-app.post("/register/:user", async (req, res) => {
+app.post("/register/:user", shallNotAuthenticated, async (req, res) => {
   let { user } = req.params;
 
   if (user == "rec") {
@@ -202,4 +225,71 @@ app.post("/register/:user", async (req, res) => {
   } else {
     res.send("Invalid URL");
   }
+});
+
+app.get("/verify", async (req, res) => {
+  res.render("verification.ejs");
+});
+app.post("/sendotp", async (req, res) => {
+  let { email, mobileno } = req.body;
+  let newOtp = Math.floor(Math.random() * 900000) + 100000;
+
+  const message = `<p style="color: red;">Hey Dear Fellow NFSUian !</p>
+<br/>
+We received a request to reset the password associated with your account. If you did not make this request, you can safely ignore this email.
+<br/><br/>
+To reset your password, please click on the <strong>Following link</strong>:
+<br/><br/>
+<a href="${"resetLink"}" >${"resetLink"}</a>
+<br/><br/>
+If clicking the link doesn't work, you can copy and paste it into your browser's address bar.
+<br/><br/>
+This link will expire on ${"expiry_time"}, so please reset your password as soon as possible.
+<br/><br/>
+<strong>Thank you,</strong><br/>
+<p style="color: red;">Qbon Team .</p>
+<br/>
+<div>
+<img
+      src="https://res.cloudinary.com/ddxv0iwcs/image/upload/v1707926485/qbon_DEV/Screenshot_2024-02-07_at_10.01.59_AM_mbeifo.png"
+      style="border-radius:2rem;width:60%;"
+      alt="..."
+    />
+</div>
+`;
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "smile.itsadil@gmail.com",
+      pass: process.env.APP_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: "ThePlacementCell@NFSU<smile.itsadil@gmail.com>",
+    to: email,
+    subject: "OTP Verification Request",
+    html: message,
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send("Failed to send OTP");
+    } else {
+      // console.log("Email sent: " + info.response);
+      // Send OTP to phone number using Twilio or another SMS service
+      // Code for sending OTP via SMS goes here
+      // res.status(200).send("OTP sent successfully");
+    }
+  });
+
+  // await client.messages.create({
+  //   body: `Your OTP is: ${newOtp}`,
+  //   from: process.env.MY_PHONE,
+  //   to: `+91${mobileno}`,
+  // });
+});
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
 });
