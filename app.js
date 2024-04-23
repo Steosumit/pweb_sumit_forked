3adc99cd807b5d962a8cbdf49d784dde9a60f949
Adil Dyer <adil@192.168.1.6>
2024-04-23 16:16:05 +0530

    get help in admin

--- app.js
+++ app.js
@@ -67,8 +67,8 @@ const sessionOptions = {
   resave: false,
   saveUninitialized: true,
   cookie: {
-    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-    maxAge: 7 * 24 * 60 * 60 * 1000,
+    expires: Date.now() + 30 * 60 * 1000,
+    maxAge: 30 * 60 * 1000,
     httpOnly: true,
   },
 };
